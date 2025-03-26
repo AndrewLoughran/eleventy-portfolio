6c0fba7e59c53c1a933437839bf5ea2a796ee510
Andrew Loughran <a.loughran90@gmail.com>
2025-03-26 15:47:56 +0000

    Update config to modify project structure

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,13 @@
 export default async function(eleventyConfig) {
 	// Configure Eleventy
+    eleventyConfig.setInputDirectory("src");
+    eleventyConfig.setOutputDirectory("dist");
+    eleventyConfig.setIncludesDirectory("_includes");
+    eleventyConfig.setLayoutsDirectory("_layouts");
 };
 
+export const config = {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+  };
