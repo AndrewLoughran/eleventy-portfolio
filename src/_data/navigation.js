f1573355badf80fea416abbff16f1523f6ce65ce
Andrew Loughran <a.loughran90@gmail.com>
2025-04-11 01:01:00 +0100

    Implement initial navigation header

---
+++ src/_data/navigation.js
@@ -0,0 +1,30 @@
+export default {
+    top: [
+      {
+        text: 'Blog',
+        url: '/blog/'
+      },
+      {
+        text: 'Credits',
+        url: '/credits/'
+      },      {
+        text: 'About',
+        url: '/about/'
+      }
+    ],
+    bottom: [
+      {
+        text: 'Style guide',
+        url: '/styleguide/'
+      },
+      {
+        text: 'Imprint',
+        url: '/imprint/'
+      },
+      {
+        text: 'Privacy',
+        url: '/privacy/'
+      }
+    ]
+  };
+  
